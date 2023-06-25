dcd0a6875728d9e3bdbb867cc7af90ce0b4aabac
nitinryali <rnssrinivas1@gmail.com>
2023-06-25 12:29:01 +0530

    Updates

---
+++ precache-manifest.a4722646281d33d4c33127b4da9c0cce.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e858a121f8aaf7066fd7d82dcb777833",
+    "url": "https/nititnryali.github.io/keepnote/index.html"
+  },
+  {
+    "revision": "a4d931c63f47251f68ff",
+    "url": "https/nititnryali.github.io/keepnote/static/js/2.260104d4.chunk.js"
+  },
+  {
+    "revision": "8c2f61ee0991217dfad4",
+    "url": "https/nititnryali.github.io/keepnote/static/js/main.85cbd257.chunk.js"
+  },
+  {
+    "revision": "0c280d41a68b38e3512f",
+    "url": "https/nititnryali.github.io/keepnote/static/js/runtime-main.ba5435d7.js"
+  }
+]);
